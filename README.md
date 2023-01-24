b53ddfb03806dc9082aaff91fe484660cfbdba4a
colemandevries <colemandevries@gmail.com>
2023-01-24 09:15:54 -0500

    updated default variables and local values

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# terraform-aws-template
-A repository template for creating reusable terraform modules to support infrastructure as code capabilities.
+# Terraform AWS Module Template
+A repository template for creating reusable terraform modules to support infrastructure as code capabilities for workloads running on AWS. 
+
+## Overview
+
+## Usage 
+
+<!-- BEGIN_TF_DOCS -->
+
+<!-- END_TF_DOCS -->
---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "app" {
+  type        = string
+  description = "the name of the application expressed as an acronym"
+  sensitive   = false
+}
+
+variable "env" {
+  type        = string
+  description = "the target tier ('dev', 'qa', 'stage', or 'prod'.)"
+  sensitive   = false
+
+  validation {
+    condition = contains(["dev", "qa", "stage", "prod"], var.env)
+  }
+}
+variable "program" {
+  type        = string
+  description = "the program associated with the application"
+  sensitive   = false
+
+  validation {
+    condition     = contains(["crdc", "ccdi", "ctos"], var.program)
+    error_message = "valid values for program are 'crdc', 'ccdi', and 'ctos'"
+  }
+}
